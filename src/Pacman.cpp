71f93aaf08801865d1aefce96dc24ce0d56a47a6
Manuel Fernando <mfer32@gmail.com>
2013-06-28 00:08:09 -0500

    Add pacman animation function.

--- src/Pacman.cpp
+++ src/Pacman.cpp
@@ -1,13 +1,14 @@
 #include "Pacman.hpp"
 
 // Constructor
-Pacman::Pacman() : lastPos() {
+Pacman::Pacman() : lastPos(), timer(), clock() {
 	Collision::CreateTextureAndBitmask(this->texture, "resources/pacmanSprites.png");
 	this->setTexture(this->texture);
 	this->setTextureRect(sf::IntRect(43, 3, 14, 14));
 	this->setPosition(sf::Vector2f(95, 205));
 
 	this->mouth = true;
+	this->setDirection(SPRITE_LEFT);
 	this->animation = true;
 	this->setFrame(0);
 	this->setSpeed(sf::Vector2f(-1,0));
@@ -39,4 +40,27 @@ void Pacman::setLastPos(sf::Vector2f lastPos) {
 
 void Pacman::setAnimation(bool animation) {
 	this->animation = animation;
+}
+
+void Pacman::startAnimation() {
+	// Animate pacman
+    if (this->getAnimation()){
+        timer = this->clock.getElapsedTime();
+        if(timer.asSeconds() > 0.04f)  {
+            this->setTextureRect(sf::IntRect(20*this->getFrame()+3, 20*this->getDirection()+3, 14, 14));
+            if (this->getFrame() == 0 && this->getMouthState()){
+                this->setFrame(1);
+            } else if(this->getFrame() == 0 && !this->getMouthState()) {
+                this->setFrame(2);
+            } else if(this->getFrame() == 1) {
+                this->setMouthState(false);
+                this->setFrame(0);
+            } else {
+                this->setTextureRect(sf::IntRect(43, 3, 14, 14));
+                this->setMouthState(true);
+                this->setFrame(0);
+            }
+            this->clock.restart();
+        }
+    }
 }
--- src/Pacman.hpp
+++ src/Pacman.hpp
@@ -9,6 +9,8 @@ private:
 
 	bool mouth; // true: pacman open the mouth, false: pacman close the mouth
     bool animation;
+    sf::Clock clock;
+    sf::Time timer;
     sf::Vector2f lastPos;
 
 public:
@@ -22,6 +24,7 @@ public:
 	void setLastPos(sf::Vector2f lastPos);
 	sf::Vector2f getLastPos();
 
+	void startAnimation();
 };
 
 #endif // __PACMAN_H__
