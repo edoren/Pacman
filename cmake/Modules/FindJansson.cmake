b01598f3d01a7d666dd1f02c05ff447b9db6a1a2
Manuel Sabogal <mfer32@gmail.com>
2014-05-03 01:43:46 -0500

    Fixed variable name error.

--- cmake/Modules/FindJansson.cmake
+++ cmake/Modules/FindJansson.cmake
@@ -2,21 +2,22 @@
 # Once done this will define
 #
 # JANSSON_FOUND - system has Jansson
-# JANSSON_INCLUDE_DIRS - the Jansson include directory
-# JANSSON_LIBRARIES - Link these to use Jansson
+# JANSSON_INCLUDE_DIR - the Jansson include directory
+# JANSSON_LIBRARY - Link these to use Jansson
 
 find_package(PkgConfig)
 pkg_check_modules(PC_JANSSON QUIET jansson)
 
-find_path(JANSSON_INCLUDE_DIR jansson.h
-          HINTS ${PC_JANSSON_INCLUDEDIR} ${PC_JANSSON_INCLUDE_DIRS}
-          PATH_SUFFIXES JANSSON)
+find_path(JANSSON_INCLUDE_DIR
+          NAMES jansson.h jansson_config.h
+          PATHS ${PC_JANSSON_INCLUDEDIR} ${PC_JANSSON_INCLUDE_DIRS})
 
-find_library(JANSSON_LIBRARY NAMES jansson
-             HINTS ${PC_JANSSON_LIBDIR} ${PC_JANSSON_LIBRARY_DIRS})
+find_library(JANSSON_LIBRARY
+             NAMES jansson
+             PATHS ${PC_JANSSON_LIBDIR} ${PC_JANSSON_LIBRARY_DIRS})
 
-set(JANSSON_LIBRARIES ${JANSSON_LIBRARY})
-set(JANSSON_INCLUDE_DIRS ${JANSSON_INCLUDE_DIR})
+set(JANSSON_LIBRARY ${JANSSON_LIBRARY})
+set(JANSSON_INCLUDE_DIR ${JANSSON_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set JANSSON_FOUND to TRUE
